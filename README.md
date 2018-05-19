5edcadfa1529346b233b3af5f50dbfdd3e96e1d6
TempleCloud <tim.langford@gmail.com>
2018-05-19 23:03:15 +0100

    Renamed and added install script.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dotvim
-Modern Vim 8+ configuration
+# dotvimi8 - Modern Vim 8+ configuration
+
+Modern Vim8 configuration using 
+
+```
+git clone git@github.com:templecloud/dotvim8.git ${HOME}/.vim
+```
+
+```
+ln -s ${HOME}/.vim/vimrc ~${HOME}/.vimrc
+ln -s ${HOME}.vim/gvimrc ${HOME}/.gvimrc
+```
---
+++ minpack-activate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install the 'minpac' Vim configuration.
+
+VIMCONFIG="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VIMRC=${HOME}/.vimrc
+
+# Create symlink to .vimrc
+if [ ! -f "${VIMRC}" ]; then
+    ln -s ${HOME}/.vim/minpack-vimrc ${HOME}/.vimrc
+else
+    echo ""${VIMRC} already exists."" 1>&2
+fi
+
+# Install minpac package manager
+if [ ! -d "${VIMCONFIG}/pack/minpac/opt/minpac" ]; then
+    mkdir -p ${VIMCONFIG}/pack/minpac/opt
+    pushd ${VIMCONFIG}/pack/minpac/opt > /dev/null
+    git clone https://github.com/k-takata/minpac.git
+    popd  > /dev/null
+else
+    pushd ${VIMCONFIG}/pack/minpac/opt/minpac > /dev/null
+    git pull
+    popd > /dev/null
+fi
+
+# Install minpack managed components.
+vim -E -c PackUpdate -c q 2> /dev/null 
+vim -E -c PackClean -c q 2> /dev/null
+
